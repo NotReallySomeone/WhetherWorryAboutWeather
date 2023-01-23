af1d007d661fb7a9ee4705354d37a55dd77122eb
NotReallySomeone <xcvrty137869585ggjhdk@gmail.com>
2023-01-23 04:39:33 -0300

    Same useless code, with useless files removed. It's like a hipster who lost his glasses.

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': '5eda972a9dmsh0447ff4f0799489p1049a7jsn7b75e5ee6f02',
+    'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com',
+  },
+}
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
---
+++ src/components/search/search.js
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+import { AsyncPaginate } from 'react-select-async-paginate'
+import { GEO_API_URL, geoApiOptions } from '../../api'
+
+const Search = ({ onSearchChange }) => {
+  const [search, setSearch] = useState(null)
+
+  const loadOptions = (inputValue) => {
+    return fetch(
+      `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`,
+      geoApiOptions
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        options: response.data.map((city) => {
+          return {
+            value: `${city.latitude} ${city.longitude}`,
+            label: `${city.name}, ${city.countryCode}`,
+          }
+        })
+      })
+      .catch((err) => console.error(err))
+  }
+
+  const handleOnChange = (searchData) => {
+    setSearch(searchData)
+    onSearchChange(searchData)
+  }
+
+  return (
+    <AsyncPaginate
+      value={search}
+      placeholder='Search for city'
+      debounceTimeout={600}
+      onChange={handleOnChange}
+      loadOptions={loadOptions}
+    />
+  )
+}
+
+export default Search
